f7cd4e2147c118e76a98c7756678734073bf1c32
CarlosDRomero <cdavid.romeror@gmail.com>
2025-08-26 10:37:25 -0500

    Creación de clase Student y Class

---
+++ src/university/Class.java
@@ -0,0 +1,57 @@
+package university;
+
+import java.util.List;
+
+public class Class {
+    String name;
+    String classRoom;
+    Teacher teacher;
+    List<Student>  students;
+
+    public Class(String name, String classRoom, Teacher teacher) {
+        this.name = name;
+        this.classRoom = classRoom;
+        this.teacher = teacher;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassRoom() {
+        return classRoom;
+    }
+
+    public void setClassRoom(String classRoom) {
+        this.classRoom = classRoom;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+    public void addStudent(Student student) {
+        this.students.add(student);
+    }
+    public void removeStudent(Student student) {
+        this.students.remove(student);
+    }
+    public void removeStudent(String id){
+        for(Student student : students){
+            if(student.getId().equals(id)){
+                students.remove(student);
+            }
+        }
+    }
+}
