29dd9f364dda7b7009b83c49c9c1015e64a6b99a
CarlosDRomero <cdavid.romeror@gmail.com>
2025-08-27 11:52:25 -0500

    Se eliminaron metodos que no se usaría en University

--- src/university/University.java
+++ src/university/University.java
@@ -32,10 +32,6 @@ public class University {
     public static void addStudent(Student s) {
         students.add(s);
     }
-    public static void addStudent(String name, String id, int age) {
-        Student student = new Student(name, id, age);
-        addStudent(student);
-    }
     public static void removeStudent(Student student) {
         students.remove(student);
     }
@@ -49,25 +45,14 @@ public class University {
     public static void addTeacher(Teacher t) {
         teachers.add(t);
     }
-    public static void addTeacher(String name, float baseSalary, float other, boolean fullTime) {
-        Teacher teacher;
-        if  (fullTime) {
-            teacher = new FullTimeTeacher(name, baseSalary, other);
-        }else{
-            teacher = new PartTimeTeacher(name, baseSalary, other);
-        }
-        addTeacher(teacher);
-    }
+
     public static void removeTeacher(Teacher teacher) {
         teachers.remove(teacher);
     }
     public static void addClass(Class c){
         classes.add(c);
     }
-    public static void addClass(String name, String classRoom) {
-        Class c = new Class(name, classRoom);
-        addClass(c);
-    }
+
     public static void removeClass(Class c) {
         classes.remove(c);
     }
